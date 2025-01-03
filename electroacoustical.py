40417df903f566448a717c0a7dacf7690058c3e3
kbasaran <kbasaran@proton.me>
2025-01-03 23:28:14 +0100

    correct licensing info

--- electroacoustical.py
+++ electroacoustical.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  6 20:23:15 2023
+# This file is part of Speaker Calculator - Loudspeaker design and calculations tool
+# Copyright (C) 2025 - Kerem Basaran
+# https://github.com/kbasaran
+__email__ = "kbasaran@gmail.com"
 
-@author: kerem
-"""
+# Linecraft is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+
+# Linecraft is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public
+# License along with Speaker Calculator. If not, see <https://www.gnu.org/licenses/>
+
 import dataclasses as dtc
 from functools import cached_property
 import numpy as np
