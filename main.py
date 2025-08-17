11538da648afcf7197aa538d14ed8fa5d0f7dee2
kbasaran <kbasaran@proton.me>
2025-08-17 21:40:13 +0200

    up version

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ import pandas as pd
 import pyperclip
 
 app_definitions = {"app_name": "Speaker Calculator",
-                   "version": "0.3.0",
+                   "version": "0.3.1",
                    "description": "Loudspeaker design and calculations",
                    "copyright": "Copyright (C) 2025 Kerem Basaran",
                    "icon_path": "images/logo2025.ico",  # relative posix path
