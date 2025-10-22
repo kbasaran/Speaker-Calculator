5ad934a259cdd11551175660adb239099dda80cf
kbasaran <kbasaran@proton.me>
2025-10-22 20:18:56 +0200

    cx-freeze fixes for good builds

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ print()
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    "packages": ["numpy", "scipy"],  # RecursionError in cx_Freeze if these are not provided
+    "packages": ["numpy", "scipy", "matplotlib", "sympy", "pandas"],  # RecursionError in cx_Freeze if these are not provided
     "include_files": files_to_include,
     "silent_level": 1,
 }
