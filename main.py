a02d7a681863246e3ce0aafa165f61cd1969b7c4
Kerem Basaran <kbasaran@users.noreply.github.com>
2026-04-05 07:53:33 +0200

    move read_wire_table to file_io

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ import matplotlib as mpl
 import numpy as np
 from functools import partial
 import electroacoustical as ac
-import pandas as pd
+import utils.file_io as fio
 import pyperclip
 
 app_definitions = {"app_name": "Speaker Calculator",
@@ -1418,32 +1418,6 @@ def get_main_dir():
         return Path(__file__).parent
 
 
-def read_wire_table(wire_table_file: Path) -> pd.DataFrame:
-
-    if not wire_table_file.exists():
-        raise FileNotFoundError(f"Wire table file not found: {wire_table_file}")
-
-    imported_wire_table = pd.read_excel(wire_table_file, "Sheet1", skiprows=range(2), index_col=0)
-    coeff_for_SI = {
-        "nominal_size": 1e-6,
-        "w_avg": 1e-6,
-        "h_avg": 1e-6,
-        "w_max": 1e-6,
-        "nominal_size": 1e-6,
-        "mass_density": 1e-3,
-        }
-    for key, coeff in coeff_for_SI.items():
-        imported_wire_table[key] = coeff * imported_wire_table[key]
-    if not imported_wire_table.index.is_unique:
-        raise IndexError("Wire names in the imported table are not unique.")
-    
-    wires_as_dict = dict()
-    for wire_name, columns_data_as_series in imported_wire_table.iterrows():
-        wires_as_dict[wire_name] = ac.Wire(name=wire_name, **columns_data_as_series.to_dict())
-
-    return wires_as_dict
-
-
 def find_feasible_coils(vals, wires):
     """Scan best matching speaker coil options."""
     try:  # try to read the N_layer_options string
@@ -1704,7 +1678,7 @@ def main():
     args = parse_args(app_definitions)
     logger = setup_logging(args=args)
     settings = Settings(app_definitions["app_name"])
-    wires = read_wire_table(get_main_dir().joinpath(settings.vc_table_file))
+    wires = fio.read_wire_table(get_main_dir().joinpath(settings.vc_table_file))
 
     # ---- Start QApplication
     if not (app := qtw.QApplication.instance()):
